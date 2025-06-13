5af34561dfdcd629e66702930a34ce4e4387ebc1
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-06-14 00:04:12 +0530

    feat: add an useeffect for fetching fake products from fakestore api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Herosection } from "./components/Herosection";
+import { Products } from "./components/Products";
 
 function App() {
-  
+  const [currentMainContent,setCurrentMainContent] = useState("Herosection");
+  const [productList,setProductList] = useState([]);
+
+  useEffect(()=>{
+      const url = "https://fakestoreapi.com/products?limit=20";
+      fetch(url)
+      .then((response)=>{
+       return response.json();
+      }) 
+      .then((result)=>{
+          console.log(result);
+          setProductList(result);
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+  },[])
+
 
   return (
     <> 
-     <Navbar/>
-    <Herosection/>
+    <Navbar/>
+    {currentMainContent === "Herosection" && <Herosection currentMainContent={currentMainContent} setCurrentMainContent={setCurrentMainContent}/>}
+    {currentMainContent==="Products" && <Products productList={productList}/>}
     </>
   );
 }
---
+++ src/components/Products.jsx
@@ -0,0 +1,12 @@
+
+import "./Products.css"
+
+function Products({productList}) {
+    return <>
+        <h2 className="products-heading">Products</h2>
+    </>
+}
+
+
+
+export {Products};
