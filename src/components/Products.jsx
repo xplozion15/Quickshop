bdeaada7ef2d7c493ad7bb98d6390bbe1c0bda15
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-06-14 12:21:41 +0530

    feat: show the products on the product page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,6 +4,21 @@ import "./Products.css"
 function Products({productList}) {
     return <>
         <h2 className="products-heading">Products</h2>
+
+        <div className="products-grid">
+            {productList.map((product)=>{
+            return  (
+                <div className="product-div" key={product.id}>
+                    <h2 className="product-name">{product.title}</h2>
+                    <img src={product.image} alt="product-image" className="product-image"/>
+                    <p className="product-price">$ {product.price}</p>
+                    <button className="add-to-bag-button">ADD TO BAG</button>
+                </div>
+
+            )
+            })}     
+        </div>  
+        
     </>
 }
 
